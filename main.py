2bef00755a0a097217c40252c7f27b4c18c0664c
kartofan <anton5veselov459@gmail.com>
2023-04-05 12:16:03 +0300

    Add some lines

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Hello world from vs code")
+import random
+string=""
+for i in range(1,11):
+    if i%2!=0:
+        string+=chr(random.randint(97,122))
+    else:
+        if i<10:
+            string+=str(i)
+        else:
+            string+=str(i-8)
+print(string)
